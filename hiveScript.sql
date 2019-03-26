2d01a2030aca4d7a5bf23ef1d610e7997459eee2
P.Suraj <srjsunny123@gmail.com>
2019-03-26 10:41:36 +0530

    sql file

--- hiveScript.sql
+++ hiveScript.sql
@@ -17,5 +17,7 @@ select * from empData limit 5;
 
 select distinct(department) from empData;
 
-select e.id,e.name,e.department, e.ctc from empData as e inner join (select e2.department,max(e2.ctc) as salary from empData as e2 group by e2.department) as t on e.department = t.department and e.ctc = t.salary;
+select e.id,e.name,e.department, e.ctc from empData as e inner join 
+(select e2.department,max(e2.ctc) as salary from empData as e2 group by e2.department) as t 
+on e.department = t.department and e.ctc = t.salary;
  
